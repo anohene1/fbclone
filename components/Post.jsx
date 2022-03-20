b4746036b2fa156d80cd73241e6345d69bd770cd
anohene1 <anoheneisaac@gmail.com>
2022-03-20 23:38:05 +0000

    render posts

---
+++ components/Post.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { ChatIcon, ShareIcon, ThumbUpIcon } from "@heroicons/react/outline";
+
+function Post({ name, message, email, image, postImage, timestamp }) {
+  return (
+    <div className="flex flex-col">
+      <div className="bg-white p-5 mt-5 rounded-t-2xl shadow-sm">
+        <div className="flex items-center space-x-2">
+          <img
+            src={image}
+            alt={name}
+            className="rounded-full"
+            width={40}
+            height={40}
+          />
+          <div>
+            <p className="font-medium">{name}</p>
+            <p className="text-xs text-gray-400">
+              {new Date(timestamp?.toDate()).toLocaleString()}
+            </p>
+          </div>
+        </div>
+
+        <p className="pt-4">{message}</p>
+      </div>
+
+      {postImage && (
+        <div className="relative h-56 md:h-96 bg-white">
+          <Image src={postImage} objectFit="cover" layout="fill" />
+        </div>
+      )}
+
+      <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <ThumbUpIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none">
+          <ChatIcon className="h-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-xs sm:text-base">Share</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,5 +1,44 @@
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import Post from "./Post";
+
 function Posts(props) {
-  return <div></div>;
+  const [realtimePosts, loading, error] = useCollection(
+    query(collection(db, "posts"), orderBy("timestamp", "desc"))
+  );
+
+  // const items = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+  // onSnapshot(items, (querySnapshot) => {
+  //   querySnapshot.forEach((doc) => {
+  //     console.log(doc.data().message);
+  //   });
+  // });
+
+  return (
+    <div>
+      {error && <strong>Error: {JSON.stringify(error)}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      {realtimePosts &&
+        realtimePosts.docs.map((post) => (
+          <Post
+            key={post.id}
+            name={post.data().name}
+            message={post.data().message}
+            email={post.data().email}
+            timestamp={post.data().timestamp}
+            image={post.data().image}
+            postImage={post.data().postImage}
+          />
+        ))}
+    </div>
+  );
 }
 
 export default Posts;
